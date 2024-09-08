a813a2dfbf6f101ee391dd931e991e151f4f0551
andsilva515 <anderson.ssilva@outlook.com>
2024-09-08 15:23:34 -0300

    conceitos iniciais

---
+++ Exercícios/Conceitos iniciais/operadores_associacao.py
@@ -0,0 +1,13 @@
+frutas = ["limao", "uva", "laranja"]
+curso = "Curso de Python"
+
+print("laranja" in frutas)
+
+print("---------------")
+
+print("Laranja" in frutas)
+print("limao" not in frutas)
+
+print("---------------")
+
+print("Python" in curso)
