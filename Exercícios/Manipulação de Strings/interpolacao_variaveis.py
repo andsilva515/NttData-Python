6bf68b5daa90d74564800c35c21a9a8629f746fd
andsilva515 <anderson.ssilva@outlook.com>
2024-09-18 22:01:08 -0300

    interpolação de variáveis

---
+++ Exercícios/Manipulação de Strings/interpolacao_variaveis.py
@@ -0,0 +1,29 @@
+nome = "José da Silva"
+idade = 33
+profissao = "Programador"
+linguagem = "Python"
+saldo = 45.444
+
+dados = {"nome": "José da Silva", "idade": 33, }
+
+print("Nome: %s, Idade: %d" % (nome, idade))
+
+print("Nome: {}, Idade: {}".format(nome, idade))
+
+print("Nome: {1}, Idade: {0} ".format(idade, nome))
+
+print("Nome: {1}, Idade: {0} Nome: {1}, {1}".format(idade, nome))
+
+print("Nome: {nome}, Idade: {idade} ".format(nome=nome, idade=idade))
+
+print("Nome: {nome} Idade: {idade}".format(**dados))
+
+
+print(f"Nome: {nome} Idade: {idade}")
+
+print(f"Nome:{nome} Idade: {idade} Saldo: {saldo}")
+
+print(f"Nome:{nome} Idade: {idade} Saldo: {saldo:10.2f}")
+
+print(f"Nome:{nome} Idade: {idade} Saldo: {saldo:.2f}")
+
