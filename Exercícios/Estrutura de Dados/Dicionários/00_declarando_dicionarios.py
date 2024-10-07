613dd72dd7b9cf194baf24bbb06756341872119b
andsilva515 <anderson.ssilva@outlook.com>
2024-10-07 19:26:42 -0300

    declarando dicionarios

---
+++ Exercícios/Estrutura de Dados/Dicionários/00_declarando_dicionarios.py
@@ -0,0 +1,11 @@
+pessoa = {"nome": "João", "idade": 33}
+print(pessoa)
+
+pessoa = dict(nome="João", idade=33)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1212" # {"nome": "João", "idade": 33, "telefone": "3333-1212"}
+print(pessoa)
+
+pessoa["nome"] = "Maria"
+print(pessoa)
