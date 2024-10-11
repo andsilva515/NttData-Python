3755693bec55e834596e96bb83ed511fc93acbbf
andsilva515 <anderson.ssilva@outlook.com>
2024-10-11 12:09:17 -0300

    implementacao com keys

---
+++ Exercícios/Estrutura de Dados/Dicionários/09_keys.py
@@ -0,0 +1,7 @@
+contatos = {"joao@gmail.com": {"nome": "Joao", "telefone": "3333-2121"}}
+
+resultado = contatos.keys() # dict_keys(['joao@gmail.com'])
+print(resultado)
+
+novo_dicionario = {"a": 100, 1: "teste", "b": "python"}
+print(novo_dicionario.keys())
