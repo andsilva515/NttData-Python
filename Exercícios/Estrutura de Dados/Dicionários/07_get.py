66a99fa2317bdb33de2fa411d576ea2210c717c8
andsilva515 <anderson.ssilva@outlook.com>
2024-10-24 21:23:40 -0300

    metodo get

---
+++ Exercícios/Estrutura de Dados/Dicionários/07_get.py
@@ -0,0 +1,14 @@
+contatos = {"joao@gmail.com": {"nome": "João", "telefone": "3333-2221"}}
+
+contatos["chave"] # keyError
+
+resultado = contatos.get("chave") # None
+print(resultado)
+
+resultado = contatos.get("chave", {})  # {}
+print(resultado)
+
+resultado = contatos.get(
+    "joao@gmail.com", {}  
+)   # {"joao@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+print(resultado)
